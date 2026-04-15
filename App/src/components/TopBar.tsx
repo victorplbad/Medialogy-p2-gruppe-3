c9cf4e8389de392cf76a159faa1e746acb4c2e95
CasperLarsen-cmyk <casper.larsen.a@gmail.com>
2026-04-15 14:47:28 +0200

    Update TopBar.tsx

--- App/src/components/TopBar.tsx
+++ App/src/components/TopBar.tsx
@@ -14,6 +14,7 @@ const TopBar: React.FC = () => {
             </button>
 
             {/*<button className="button" aria-label="Video liste" onClick={() => switchMenu("search_results")}>*/}
+            {/*Yarr!*/}
             {/*</button>*/}
 
             {/* Søgefelt — højre */}
