864bf4769b5cc1d9b190642a50139082f0d32438
victorplbad <84019887+victorplbad@users.noreply.github.com>
2026-04-20 12:02:47 +0200

    go to stats button

--- App/src/components/TopBar.tsx
+++ App/src/components/TopBar.tsx
@@ -1,7 +1,7 @@
 // src/components/TopBar.tsx
 import React, { useState } from "react";
 import "./TopBar.css";
-import switchMenu from "../GlobalFunctions.tsx";
+import switchMenu from "../GlobalFunctions.tsx";    
 
 const TopBar: React.FC = () => {
     const [query, setQuery] = useState("");
@@ -11,6 +11,9 @@ const TopBar: React.FC = () => {
             {/* Gear ikon — venstre */}
             <button className="topbar__icon-btn" aria-label="Indstillinger" onClick={() => switchMenu("settings")}>
                 <GearIcon />
+            </button>
+            <button className="topbar__icon-btn" aria-label="Indstillinger" onClick={() => switchMenu("stats")}>
+                <HouseIcon />
             </button>
 
             {/*<button className="button" aria-label="Video liste" onClick={() => switchMenu("search_results")}>*/}
@@ -46,6 +49,22 @@ export default TopBar;
 
 const GearIcon = () => (
     <svg width="22" height="22" viewBox="0 0 24 24" fill="none"
+        stroke="currentColor" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round">
+        <circle cx="12" cy="12" r="3" />
+        <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1-2.83 2.83l-.06-.06
+      a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-4 0v-.09
+      A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83-2.83
+      l.06-.06A1.65 1.65 0 0 0 4.68 15a1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1 0-4h.09
+      A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 2.83-2.83
+      l.06.06A1.65 1.65 0 0 0 9 4.68a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 4 0v.09
+      a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 2.83
+      l-.06.06A1.65 1.65 0 0 0 19.4 9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 0 4h-.09
+      a1.65 1.65 0 0 0-1.51 1z" />
+    </svg>
+);
+
+const HouseIcon = () => (
+    <svg width="36" height="22" viewBox="0 0 24 24" fill="none"
         stroke="currentColor" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round">
         <circle cx="12" cy="12" r="3" />
         <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1-2.83 2.83l-.06-.06
