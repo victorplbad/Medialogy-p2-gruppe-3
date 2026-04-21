ad71871b7bf9da6182a411d33b8422554e31022a
Anton Bressendorff <126203682+AntonBressendorff@users.noreply.github.com>
2026-04-21 12:19:30 +0200

    testing stuff

--- App/src/App.tsx
+++ App/src/App.tsx
@@ -3,13 +3,16 @@ import TopBar from "./components/TopBar";
 import "./App.css";
 
 function App() {
-  const [page, setPage] = useState(0);
+  const [page, setPage] = useState<0 | 1>(0);
+  const [direction, setDirection] = useState<"up" | "down">("down");
 
   const touchStartY = useRef<number | null>(null); 
   const isAnimating = useRef(false);
 
-  const goToPage = (target: number) => {
+  const goToPage = (target: 0 | 1, dir?: "up" | "down") => {
     if (isAnimating.current) return;
+
+    setDirection(dir ?? (target > page ? "down" : "up"));
 
     isAnimating.current = true;
     setPage(target);
@@ -21,8 +24,12 @@ function App() {
 
   // PC control - ScrollWheel
   const handleWheel = (e: React.WheelEvent) => {
-    if (page === 1 && e.deltaY < -40) {
-      goToPage(0); // scroll up to go back
+    if (page === 1 && e.deltaY < 0) {
+      goToPage(0, "up"); 
+    }
+    if (page === 1 && e.deltaY > 0) {
+      goToPage(0, "down");
+      // alert("hej");
     }
   };
 
@@ -61,20 +68,26 @@ function App() {
       <TopBar/>
       <div
         className="slider"
-        style={{ transform: `translateY(-${page * 100}vh)` }}
+        // style={{ transform: `translateY(-${page * 100}vh)` }}
       >
         {/* PAGE 1 */}
-        <div className="page choice-page">
+        <div className={`page choice-page ${
+          page === 0 
+            ? "active"
+            : direction === "up"
+            ? "enter-bottom"
+            : "enter-top"
+          }`}
+        >
           <div className="grid">
             {[...Array(6)].map((_, i) => (
               <div
                 key={i}
                 className="portrait"
                 onPointerDown={() => {
-                  setTimeout(() => {
-                    setPage(1);
-                  }, 120); //delay on click
-                  }}
+                    goToPage(1, "down");
+                  }
+                }
               >
                 <span>Thumbnail {i + 1}</span>
               </div>
@@ -83,8 +96,16 @@ function App() {
         </div>
 
         {/* PAGE 2 */}
-        <div className="page video-page">
+        <div className={`page video-page ${
+          page === 1 
+            ? "active" 
+            : direction === "up"
+            ? "enter-bottom"
+            : "enter-top"
+          }`}
+        >
           <h1>Video Page (placeholder)</h1>
+          
         </div>
       </div>
     </div>
