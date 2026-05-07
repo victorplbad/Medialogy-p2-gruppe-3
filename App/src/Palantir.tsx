1b74d737fe746a8945d7b516a8db7c233ef63596
CasperLarsen-cmyk <casper.larsen.a@gmail.com>
2026-05-07 11:38:01 +0200

    Update Palantir.tsx

--- App/src/Palantir.tsx
+++ App/src/Palantir.tsx
@@ -3,17 +3,16 @@ function LogWatchTime(startTime: Date, videoDuration: number) {
     const now = new Date();
     const todayString = TodayString()
 
+    const vNum = parseInt(localStorage.getItem(todayString + "vNum") || "0") || 0;
     const timeWatched: number = now.getTime() - startTime.getTime();
     const percentWatched = timeWatched / (videoDuration * 1000);
-    console.log(timeWatched, videoDuration, percentWatched);
     const timeWatchedToday = parseInt(localStorage.getItem(todayString) || "0") || 0;
-    const vNum = parseInt(localStorage.getItem(todayString + "vNum") || "0") || 0;
     const smol = {
         watchTime: timeWatched,
         progress: percentWatched,
     }
+
     localStorage.setItem(todayString + "vNum", (vNum + 1).toString());
-    //localStorage.setItem(todayString + "v" + vNum, timeWatched.toString());
     localStorage.setItem(todayString + "v" + vNum, JSON.stringify(smol));
     localStorage.setItem(todayString, (timeWatchedToday + timeWatched).toString());
 }
@@ -44,47 +43,44 @@ function DayTime() {
     return parseInt(localStorage.getItem(TodayString()) || "0") / 1000;
 }
 
-interface dataExport {
-    [date: string]: DaySummary
-}
-
 function exportData() {
     const object: dataExport = {};
 
     for (let i = 0; i < localStorage.length; i++) {
         const key = localStorage.key(i) || "";
-        const regex = /[a-z]+/gid.exec(key);
+        const regex = /[a-z]+/gid.exec(key);                        //Regular expression matching first instance of 1 or more letters
         let date = key
-        if (regex !== null) date = key.substring(0, regex.index)
+        if (regex !== null) date = key.substring(0, regex.index)    //if there is a letter the part before that letter will be the date
 
-        console.log(object[date]);
-        if (object[date] === undefined) object[date] = {
+        if (object[date] === undefined) object[date] = {            //Creaty empty "DaySummary" if none with this day exists
             sessionTimes: [],
             videoWatchTimes: [],
             videoProgress: [],
         };
-
-        console.log(object);
-        if (regex === null) { //This is a day watch time summary
+        
+        if (regex === null) {                                       //This is a day watch time summary
             object[date].date = key
             object[date].totalWatchTime = parseInt(localStorage.getItem(key) || "");
-        } else if (regex[0] == "v") {
+        } else if (regex[0] == "v") {                               //Video watchtime and percentage watched
             const JSon = JSON.parse(localStorage.getItem(key) || "{}");
             object[date].videoWatchTimes.push(JSon.watchTime)
             object[date].videoProgress.push(JSon.progress)
-        } else if (regex[0] == "vNum") {
+        } else if (regex[0] == "vNum") {                            //Number of videos in a day
             object[date].totalVideos = parseInt(localStorage.getItem(key) || "");
-        } else if (regex[0] == "s") {
+        } else if (regex[0] == "s") {                               //Length of a session
             object[date].sessionTimes.push(parseInt(localStorage.getItem(key) || ""));
-        } else if (regex[0] == "sNum") {
+        } else if (regex[0] == "sNum") {                            //Number of sessions this day
             object[date].totalSessions = parseInt(localStorage.getItem(key) || "");
         }
     }
 
     const JSonString = JSON.stringify(object);
     navigator.clipboard.writeText(JSonString);
-
     alert(JSonString);
+}
+
+interface dataExport {
+    [date: string]: DaySummary
 }
 
 type DaySummary = {
